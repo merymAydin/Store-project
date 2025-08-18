93f96c00237fd300dbddcdcaab492670c56b26d9
Meryem Aydın <aydnn.meryem@gmail.com>
2025-08-18 11:17:57 +0300

    Add Header and HeaderButton components with styling
    - Created Header component with a fixed position and title.
    - Implemented HeaderButton component for adding items to the cart.
    - Added CSS styles for Header and HeaderButton, including hover and active states.
    - Included animations for cart button interactions.

---
+++ Store-Project/src/Components/Layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+import HeaderButton from './HeaderButton'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <h1>Meryem's Store</h1>
+        <HeaderButton/>
+      </div>
+    </div>
+  )
+}
+
+export default Header
